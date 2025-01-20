c3e35c2210fd324990b5ee71018e5558318129d1
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-20 17:15:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 //your JS code here. If required.
+
+
+	let tr=document.createElement('tr');
+	tr.innerHTML=`<td colspan=2 >Loading...</td>`;
+	let output=document.querySelector('#output');
+	output.appendChild(tr);
+	let promise1=()=>{
+		return new Promise((res,rej)=>{
+			let r=Math.floor(Math.random()*3+1);
+			setTimeout(()=>{
+				res(r);
+			},r*1000);
+		})
+	}
+	let promise2=()=>{
+		return new Promise((res,rej)=>{
+			let r=Math.floor(Math.random()*3+1);
+			setTimeout(()=>{
+				res(r);
+			},r*1000);
+		})
+	}
+	let promise3=()=>{
+		return new Promise((res,rej)=>{
+			let r=Math.floor(Math.random()*3+1);
+			setTimeout(()=>{
+				res(r);
+			},r*1000);
+		})
+	}
+
+	Promise.all([promise1(),promise2(),promise3()])
+	.then((data)=>{
+      output.innerHTML='';
+      let total=0;
+      data.forEach((el,i)=>{
+        total+=el;
+        let tr=document.createElement('tr');
+          tr.innerHTML=`<td>Promise ${i+1}</td>
+                        <td>${el}</td>`;
+        output.appendChild(tr);
+      })
+       output.innerHTML+=`<tr><td>Total</td>
+                        <td>${total}</td></tr>`;
+
+	})
+
+
+
+	
+
+
+
+
+
+
+
